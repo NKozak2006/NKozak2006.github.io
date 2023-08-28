c9c7eac8a60f6388bba939289fb51c2c8eafe0ad
NKozak2006 <123200571+NKozak2006@users.noreply.github.com>
2023-08-28 05:07:36 -0500

    Update script.js

--- please_work/dist/script.js
+++ please_work/dist/script.js
@@ -45,11 +45,11 @@ $(function () {
     ],
     albumArtworks = ["_1", "_2", "_3", "_4", "_5"],
     trackUrl = [
-      "https://nkozak2006.github.io/please_work/src/music/folklore/the_1.mp3",
-      "https://nkozak2006.github.io/please_work/src/music/folklore/cardigan.mp3",
-      "https://nkozak2006.github.io/please_work/src/music/folklore/the_last_great_american_dynasty.mp3",
-      "https://nkozak2006.github.io/please_work/src/music/folklore/exile.mp3",
-      "https://nkozak2006.github.io/please_work/src/music/folklore/my_tears_ricochet.mp3"
+      "https://nkozak2006.github.io/please_work/src/Music/folklore/the_1.mp3",
+      "https://nkozak2006.github.io/please_work/src/Music/folklore/cardigan.mp3",
+      "https://nkozak2006.github.io/please_work/src/Music/folklore/the_last_great_american_dynasty.mp3",
+      "https://nkozak2006.github.io/please_work/src/Music/folklore/exile.mp3",
+      "https://nkozak2006.github.io/please_work/src/Music/folklore/my_tears_ricochet.mp3"
     ],
     playPreviousTrackButton = $("#play-previous"),
     playNextTrackButton = $("#play-next"),
