7f58973754b5c7181d9e99cacc2167af2bebcc26
NKozak2006 <ndkz1111nate@gmail.com>
2023-08-28 14:43:03 -0500

    change

--- player/folklore/dist/script.js
+++ player/folklore/dist/script.js
@@ -65,7 +65,7 @@ $(function () {
       "Taylor Swift - betty",
       "Taylor Swift - peace",
       "Taylor Swift - hoax",
-      "Taylor Swift - the lakes - bonus track"
+      "Taylor Swift - the lakes (bonus track)"
     ],
     albumArtworks = ["_1", "_2", "_3", "_4", "_5", "_6", "_7", "_8", "_9", "_10", "_11", "_12", "_13", "_14", "_15", "_16", "_17"],
     trackUrl = [
